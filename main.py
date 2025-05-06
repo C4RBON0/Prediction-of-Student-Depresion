11f488b0f2e9448343c887fe708d4c7cbff5ae07
Angel Cervera Ronda <angelcerveraronda@gmail.com>
2025-05-06 19:19:13 +0200

    charge dataset

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ import graphviz
 #To install the required libraries, uncomment the following line and run it in your terminal:
 #pip install --user -r requirements.txt
 
-
 print("Libraries imported successfully")
 # Load the dataset
+
+#charge dataset
+dataset = pd.read_csv('student_depression_dataset.csv')
+
